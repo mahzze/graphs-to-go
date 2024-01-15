62845f0ec42431a7d8146651c28974a80c97e9b3
mah <zanzinzzo@gmail.com>
2024-01-15 15:58:53 -0300

    refactor: change structure of the directed graph + create AddNode function
    The prior structure did not take the edges into consideration, storing only the nodes of the graph, and not the edges, making it not have an adjacency matrix
    Also, maybe it's a good idea to standardize some names across the project, e.g using only `vertices` or `nodes`.

--- multidirectional-data-structure/graph.go
+++ multidirectional-data-structure/graph.go
@@ -1,6 +1,15 @@
 package directed_graph
 
+import "fmt"
+
 // The directional graph is  weighted and represented by a an adjacency matrix.
 type DirectionalGraph struct {
-	nodes [][]*DirectedNode
+	nodes []*DirectedNode
+	edges [][]int
+}
+
+func (dg *DirectionalGraph) Print() {
+	for _, v := range dg.nodes {
+		fmt.Printf("= node key: %v -- inNeighbors : %v\n= outNeighbors: %v\n", v.key, v.InNeighbors, v.OutNeighbors)
+	}
 }
--- multidirectional-data-structure/node.go
+++ multidirectional-data-structure/node.go
@@ -1,5 +1,7 @@
 package directed_graph
 
+import "fmt"
+
 // Type of a vertex in a directed graph
 type DirectedNode struct {
 	key          int
@@ -7,21 +9,36 @@ type DirectedNode struct {
 	InNeighbors  []*DirectedNode
 }
 
-// AddNode adds a node. It works by copying the adjacency matrix into a new matrix
-// with the new node and returning the new matrix, so beware of memory usage.
+// AddNode adds a node. This increases the space used by the adjacency matrix
+// at a quadratic rythm, manner, so beware of memory usage.
+func (dg *DirectionalGraph) AddNode(k int) {
+	// checks to see if the key is already in use
+	for _, node := range dg.nodes {
+		if k == node.key {
+			err := fmt.Errorf("Vertex %v not added because it uses an existing key", k)
+			fmt.Println(err.Error())
+		}
+	}
+	dg.nodes = append(dg.nodes, &DirectedNode{key: k})
+}
 
 // PruneNode removes a node regardless of wether it has any edges or not.
+// If it has, the nodes are also removed.
+func (dg *DirectionalGraph) PruneNode(k int) {
+
+}
 
 // RemoveNode removes a node, but only if the node has no edges. To
 // remove regardless of the edges, use PruneNode().
+func (dg *DirectionalGraph) RemoveNode(k int) {
+
+}
 
 // GetNode
-func (g *DirectionalGraph) GetNode(k int) *DirectedNode {
-	for x := range g.nodes {
-		for y := range g.nodes[x] {
-			if k == g.nodes[x][y].key {
-				return g.nodes[x][y]
-			}
+func (dg *DirectionalGraph) GetNode(k int) *DirectedNode {
+	for _, node := range dg.nodes {
+		if k == node.key {
+			return node
 		}
 	}
 	return nil
